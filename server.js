a008d12e0582b498e123b328cf0661d1ce9670cc
keyelluna <lunakimlouren.a@gmail.com>
2026-03-06 11:23:17 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,51 @@
+//API FRAMEWORK
+const express = require('express');
+//CROSS ORIGIN RESOURCE SHARING
+const cors = require('cors');
+//ENVIRONMENT VARIABLES
+require ('dotenv').config();
+//DATABASE CONNECTION
+// const db = require('./config/db.js');
+//ROUTES
+const routes = require('./routes/index')
+
+//UTILIZATION IOF EXPRESS
+
+// 1. Define your allowed origins
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'https://productdrill.netlify.app'
+]; 
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+
+
+const app = express();
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//USE ROUTES
+app.use('/api', routes)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is runnimg on Port ${process.env.PORT}`)
+})
+
+
+
+
+
